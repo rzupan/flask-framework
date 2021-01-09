3d3317304b1c7204d0d972463e74a0e6386c8356
rzupan <zupanrobert1991@gmail.com>
2021-01-09 15:25:31 -0500

    Attempt13

--- app.py
+++ app.py
@@ -2,6 +2,28 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+def plotInfo(ticker):
+
+	import pandas as pd
+	from bokeh.plotting import figure, output_file, save
+	import requests
+
+	key = 'YY749BF6ETJWL2A7'
+
+	url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&&symbol={}&apikey={}'.format(ticker, key)
+
+	response = requests.get(url)
+
+	df = pd.DataFrame(response.json()['Time Series (Daily)'])
+
+	p = figure(title=ticker+" Close Price", x_axis_type='datetime', x_axis_label='Date', y_axis_label='Price')
+
+	p.line(df.loc['4. close',:].index.values.astype('datetime64[ns]'), df.loc['4. close',:].values.astype('float64'), legend_label=ticker+" Close Price", line_width=2)
+
+	save(p,"templates/Graph.html")
+
+	return p
+
 
 @app.route('/')
 def index():
@@ -12,40 +34,20 @@ def index():
 def about():
   return render_template('about.html')
 
-@app.route('/Graph', methods=['POST'])
+@app.route('/Graph', methods=['GET', 'POST'])
 def graph():
 
-	import pandas as pd
-	import numpy as np
-	from bokeh.plotting import figure, output_file, show, save
-	from bokeh.io import output_file
-	from bokeh.resources import CDN
-	from bokeh.embed import file_html
-
-	import requests
-	import simplejson as json
-
-	key = 'YY749BF6ETJWL2A7'
+	from bokeh.embed import components
 	ticker = request.form['ticker']
-	print(ticker)
-	url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&&symbol={}&apikey={}'.format(ticker, key)
+	p = plotInfo(ticker)
 
-	response = requests.get(url)
+	script, div =components(p)
+	print(script)
+	print(div)
+	kwargs = {'script':script,'div':div}
+	kwargs['title'] = 'Stock Display'
 
-	output_file("templates/Graph.html")
-
-	df = pd.DataFrame(response.json()['Time Series (Daily)'])
-
-	p = figure(title=ticker+" Close Price", x_axis_type='datetime', x_axis_label='Date', y_axis_label='Price')
-
-	p.line(df.loc['4. close',:].index.values.astype('datetime64[ns]'), df.loc['4. close',:].values.astype('float64'), legend_label=ticker+" Close Price", line_width=2)
-
-	print(p.line)
-	print(df.loc['4. close',:].values[0])
-
-	save(p,filename="templates/Graph.html")
-
-	return render_template('Graph.html')
+	return render_template('Graph.html',**kwargs)
 
 if __name__ == '__main__':
   app.run(port=33507)
